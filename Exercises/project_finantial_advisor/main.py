d76b47055e8bc48aa4c8f53daef25487ea6d3b1f
Sandra Queiroz <sandravivan@gmail.com>
2025-12-05 22:06:14 -0800

    Add Welcome Screen

---
+++ Exercises/project_finantial_advisor/main.py
@@ -0,0 +1,21 @@
+def show_welcome_screen():
+    print("=====================================")
+    print ("Welcome to Your Financial Planning")
+    print("=====================================")
+    print()
+    print("Thank you for choosing to begin your financial journey with us.")
+    print("We will guide you step-by-step through your initial assessment,")
+    print("including personal details, current finances, and future goals.")
+    print()
+    input("Press ENTER to start your onboarding...")
+    print()
+
+def show_welcome_screen()
+    
+if __name__ == "__main__":
+    main()
+
+
+
+
+
